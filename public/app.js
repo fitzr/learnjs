b01d1abc6398455684d2c38f835f3b02c1b01e47
fitzr <fitzroy.t.hagi@gmail.com>
2017-07-04 22:25:01 +0900

    :sparkles: add data bindings

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
 'use strict'
 
 class LearnJS {
-  static problemView(problemNumber) {
-    const title = `Problem #${problemNumber} Coming soon!`
-    return $('<div class="problem-view">').text(title)
+
+  static appOnReady() {
+    window.onhashchange = () => {
+      LearnJS.showView(window.location.hash)
+    }
+    LearnJS.showView(window.location.hash)
   }
 
   static showView(hash) {
@@ -17,12 +20,31 @@ class LearnJS {
     }
   }
 
-  static appOnReady() {
-    window.onhashchange = () => {
-      LearnJS.showView(window.location.hash)
-    }
-    LearnJS.showView(window.location.hash)
+  static problemView(data) {
+    const problemNumber = parseInt(data, 10)
+    const view = $('.templates .problem-view').clone()
+    view.find('.title').text(`Problem #${problemNumber}`)
+    LearnJS.applyObject(LearnJS.problems[problemNumber - 1], view)
+    return view
+  }
+
+  static applyObject(obj, elem) {
+    Object.entries(obj).forEach(([key, val]) => {
+      elem.find(`[data-name='${key}']`).text(val)
+    })
   }
 }
+
+LearnJS.problems = [
+  {
+    description: 'What is truth?',
+    code: 'function problem() { return __; }'
+  },
+  {
+    description: 'Simple Math',
+    code: 'function problem() { return 42 === 6 * __; }'
+  },
+]
+
 
 const learnjs = LearnJS
