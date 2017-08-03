138fde3efa7ccf777950d383906c254b7db2cf65
fitzr <fitzroy.t.hagi@gmail.com>
2017-08-03 23:57:30 +0900

    :sparkles: add popularAnswers()

--- services/lib/index.js
+++ services/lib/index.js
@@ -1,3 +1,45 @@
-exports.echo = function(json, context) {  
-  context.succeed(["Hello from the cloud! You sent " + JSON.stringify(json)]);
-};
+
+const AWS = require('aws-sdk')
+
+AWS.config.region = 'ap-northeast-1'
+
+exports.dynamodb = new AWS.DynamoDB.DocumentClient()
+
+const scan = (param) => (
+  new Promise((resolve, reject) => {
+    exports.dynamodb.scan(param, (err, data) => { err ? reject(err) : resolve(data) })
+  })
+)
+
+const filterTopFive = (items) => {
+  const countMap = items.reduce((obj, { answer }) => {
+    obj[answer] = (obj[answer] || 0) + 1
+    return obj
+  }, {})
+
+  return Object
+    .keys(countMap)
+    .map(key => [key, countMap[key]])
+    .sort((e1, e2) => e1[1] - e2[1])
+    .slice(0, 5)
+    .reduce((ret, [answer, count]) => {
+      ret[answer] = count
+      return ret
+    }, {})
+}
+
+exports.popularAnswers = (json, context) => (
+  scan({
+    FilterExpression: 'problemId = :problemId',
+    ExpressionAttributeValues: { ':problemId': json.problemNumber },
+    TableName: 'learnjs'
+  })
+    .then(data => { context.succeed(filterTopFive(data.Items)) })
+    .catch(err => { context.fail(err) })
+)
+
+
+exports.echo = (json, context) => {
+  context.succeed(["Hello from the cloud! You sent " + JSON.stringify(json)])
+}
+
--- services/spec/index_spec.js
+++ services/spec/index_spec.js
@@ -1,11 +1,11 @@
 
-
 describe('lambda function', () => {
   const index = require('../lib/index')
   let context
 
   beforeEach(() => {
-    context = jasmine.createSpyObj('context', ['succeed'])
+    context = jasmine.createSpyObj('context', ['succeed', 'fail'])
+    index.dynamodb = jasmine.createSpyObj('dynamo', ['scan'])
   })
 
   describe('echo', () => {
@@ -13,6 +13,32 @@ describe('lambda function', () => {
       index.echo({}, context)
       const expected = ["Hello from the cloud! You sent {}"]
       expect(context.succeed).toHaveBeenCalledWith(expected)
+    })
+  })
+
+  describe('popularAnswers', () => {
+    it('requests problems with the given problem number', () => {
+      index.popularAnswers({problemNumber: 42}, context)
+      expect(index.dynamodb.scan).toHaveBeenCalledWith({
+        FilterExpression: 'problemId = :problemId',
+        ExpressionAttributeValues: { ':problemId': 42 },
+        TableName: 'learnjs',
+      }, jasmine.any(Function))
+    })
+
+    it('groups answers by minified code', (done) => {
+      const promise = index.popularAnswers({problemNumber: 1}, context)
+      index.dynamodb.scan.calls.first().args[1](undefined, {Items: [
+        {answer: 'true'},
+        {answer: 'true'},
+        {answer: 'true'},
+        {answer: '!false'},
+        {answer: '!false'},
+      ]})
+      promise.then(() => {
+        expect(context.succeed).toHaveBeenCalledWith({'true': 3, '!false': 2})
+        done()
+      })
     })
   })
 })
